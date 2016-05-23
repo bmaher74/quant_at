ea5e9cd666a6b2a5003f55f00e5332c77eb49e8f
Burak <burak@bla.com>
2016-05-23 15:05:06 +0300

    -

--- data/trial.py
+++ data/trial.py
@@ -2,42 +2,36 @@ import pandas as pd
 import numpy as np 
 import simple
 
-#px = simple.get('DJIA')
-
-col = 'PRICE'
-ff = 'c:/Users/burak/Downloads/pysystemtrade/sysdata/legacycsv/AUD_price.csv'
+#col = 'a'; px = simple.get('IYH')
+col = 'PRICE'; ff = 'c:/Users/burak/Downloads/pysystemtrade/sysdata/legacycsv/LEANHOG_price.csv'
 px = pd.read_csv(ff,parse_dates=True,index_col=0)
-df_yhoo = px
+
+lev = 1.
 
 signals = pd.DataFrame(index=px.index) 
 signals['signal'] = 0 
-
 short_ma = pd.rolling_mean(px[col], 40, min_periods=1) 
 long_ma = pd.rolling_mean(px[col], 100, min_periods=1) 
 signals['signal'] = np.where(short_ma > long_ma, 1, 0) 
 px['signal'] = signals['signal'].shift(1) 
 px['ret'] = px[col].pct_change() * px['signal']
-ret = px.ret.dropna()
+ret = px.ret.dropna() * lev
 cumret=np.cumprod(1+ret)-1
 print 'APR', ((np.prod(1.+ret))**(252./len(ret)))-1
 print 'Sharpe', np.sqrt(252.)*np.mean(ret)/np.std(ret)
 
-#df_yhoo = pd.read_csv("./legacycsv/CORN_price.csv",parse_dates=True,index_col=0)
-signals = pd.DataFrame(index=df_yhoo.index) 
+signals = pd.DataFrame(index=px.index) 
 signals['signal'] = np.nan
-middle = pd.rolling_mean(df_yhoo[col], 40, min_periods=1) 
-std = pd.rolling_std(df_yhoo[col], 40, min_periods=1)
-
-df_yhoo['middle'] = middle
-df_yhoo['top'] = middle+2*std
-df_yhoo['bottom'] = middle-2*std
-df_yhoo[[col,'middle','bottom','top']].plot()
-
-signals['signal'] = np.where(df_yhoo[col] > middle+2*std, -1, np.nan) 
-signals['signal'] = np.where(df_yhoo[col] < middle-2*std, 1, np.nan)
+middle = pd.rolling_mean(px[col], 40, min_periods=1) 
+std = pd.rolling_std(px[col], 40, min_periods=1)
+px['middle'] = middle
+px['top'] = middle+2*std
+px['bottom'] = middle-2*std
+signals['signal'] = np.where(px[col] > middle+2*std, -1, np.nan) 
+signals['signal'] = np.where(px[col] < middle-2*std, 1, np.nan)
 signals['signal'] = signals['signal'].fillna(method='ffill')
-df_yhoo['ret'] = df_yhoo[col].pct_change() * signals['signal'].shift(1)
-ret = df_yhoo.ret.dropna()
+px['ret'] = px[col].pct_change() * signals['signal'].shift(1)
+ret = px.ret.dropna() * lev
 cumret=np.cumprod(1+ret)-1
 print 'APR', ((np.prod(1.+ret))**(252./len(ret)))-1
 print 'Sharpe', np.sqrt(252.)*np.mean(ret)/np.std(ret)
