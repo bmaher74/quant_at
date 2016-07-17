07e0dcd001522e26a789a0357789c1ebf125a037
Burak <burak@bla.com>
2016-07-17 21:37:01 +0300

    -

--- data/test/test_quandl.py
+++ data/test/test_quandl.py
@@ -3,11 +3,12 @@ import Quandl, os
 fname = '%s/.quandl' % os.environ['HOME']
 auth = open(fname).read()
 
-df = Quandl.get("CME/CLV2008", 
+df = Quandl.get("CME/CLX2010", 
+#df = Quandl.get("CME/EDX2012", 
                 trim_start="2001-01-01",
                 trim_end="2020-01-01",
                 returns="pandas",
                 authtoken=auth)
 
 print df
-df.to_csv("out.csv")
+df.to_csv("out10.csv")
