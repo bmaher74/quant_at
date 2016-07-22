ab449935db9d299b0f74cfe0fcf8e12c1ce38935
Burak <burak@bla.com>
2016-07-22 11:47:07 +0300

    -

--- data/futures.py
+++ data/futures.py
@@ -274,15 +274,22 @@ if __name__ == "__main__":
     simple.check_mongo()    
     f = '%(asctime)-15s: %(message)s'
     logging.basicConfig(filename='%s/futures.log' % os.environ['TEMP'],level=logging.DEBUG, format=f)
-    
-    if len(sys.argv) == 3:
-        if sys.argv[1] == "--get-load-contract":
-            print sys.argv[2]
-            download_and_save(work_items=[('CME','CL','G',1984,'1980-01-01')],
-                              str_end='2016-01-01',
-                              futures=db.futures)
+    if len(sys.argv) == 4:
+        if sys.argv[1] == "--load-save-cnt":
+            """
+            Download from web and load a single contract, example usage is
+            futures.py --load-save-cnt CME CLX2004
+            """
+            x1,x1,market, cnt = sys.argv
+            year=cnt[-4:]; mon=cnt[-5]; code=cnt[:-5]
+            connection = MongoClient()
+            findb = connection["findb"].futures
+            download_and_save(work_items=[(market,code,mon,int(year),'1900-01-01')],db=findb)
             
     elif len(sys.argv) == 2:
+        """
+        Simply get the latest
+        """
         if sys.argv[1] == "--latest":
             print 'latest'
             download_data()
