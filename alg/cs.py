5e8900aa80b9fae5b0bd9b4e161d9cc27de1e847
Burak <burak@bla.com>
2016-08-01 14:24:57 +0300

    -

--- alg/cs.py
+++ alg/cs.py
@@ -6,20 +6,8 @@ import matplotlib.pyplot as plt
 
 insts = pd.read_csv('instruments.csv',index_col=[0,1],comment='#').to_dict()
 
-#inst = "FV"
-inst = "CL"; market="CME"
-rollcycle = insts['rollcycle'][inst,market]
-rolloffset = insts['rolloffset'][inst,market]
-expday = insts['expday'][inst,market]
-expmon = insts['expmon'][inst,market]
-carryoffset = insts['carryoffset'][inst,market]
-print inst, market, rollcycle, rolloffset, expday, expmon, carryoffset
-ctd = futures.get_contracts(market,inst,1990,futures.systemtoday().year)
-cts_assigned = futures.which_contract(inst, ctd, rollcycle, rolloffset, expday, expmon)
-df_carry = futures.create_carry(cts_assigned[pd.isnull(cts_assigned.effcont)==False],int(carryoffset),ctd)
-df_stitched = futures.stitch_contracts(cts_assigned, ctd, 's')
-df_stitched.plot()
-plt.savefig('misc_01.png')
+inst = "FV"; market = "CME"
 
+futures.combine_contract_info_save(inst, market, insts, db="findb")
 
 
