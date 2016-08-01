bd9f2a5826eef203dd22f27f154dd93a851b3cc6
Burak <burak@bla.com>
2016-08-01 14:38:50 +0300

    -

--- alg/cs.py
+++ alg/cs.py
@@ -5,12 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 insts = pd.read_csv('instruments.csv',index_col=[0,1],comment='#').to_dict('index')
-rollcycle = insts[("CL","CME")]['rollcycle']
-print rollcycle
-exit()
 
-for (sym,market) in insts.to_dict('index').keys(): 
+for (sym,market) in insts.keys(): 
     print sym, market
-    futures.combine_contract_info_save(inst, market, insts.to_dict(), db="findb")
+    futures.combine_contract_info_save(inst, market, insts, db="findb")
     
-   
